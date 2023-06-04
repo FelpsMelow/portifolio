1022909c990cdc880641012b31f64f3e0179801e
Felipe Melo <felipemelo.unidade@gmail.com>
2023-06-03 22:42:51 -0300

    adicionando o menu responssivo

---
+++ public/assets/js/index.js
@@ -0,0 +1,9 @@
+function show_menu_bar () {
+    var elemento = document.querySelector(".responsive_menu")
+    elemento.style.display = "flex"
+}
+
+function hide_menu_bar () {
+    var elemento = document.querySelector(".responsive_menu")
+    elemento.style.display = "none"
+}
