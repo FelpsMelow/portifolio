51490263af208ce920199d6edd92551172a2c277
Felipe Melo <felipemelo.unidade@gmail.com>
2023-06-08 20:20:18 -0300

    colocando o dark_mode e light_mode de maneira permanente

--- public/assets/js/common/change_color_thema.js
+++ public/assets/js/common/change_color_thema.js
@@ -2,10 +2,31 @@ const btn_mudar_tema = document.querySelector('.tema')
 btn_mudar_tema.addEventListener('click', () => {
     let html = document.querySelector('.html')
     html.classList.toggle('light_mode')
+
+    if (html.classList.contains('light_mode')) {
+        localStorage.setItem('theme', 'light_mode')
+    } else {
+        localStorage.setItem('theme', 'dark_mode')
+    }
+
 })
 
 const btn_mudar_tema_menu = document.querySelector('.tema_menu')
 btn_mudar_tema_menu.addEventListener('click', () => {
     let html = document.querySelector('.html')
     html.classList.toggle('light_mode')
+
+    if (html.classList.contains('light_mode')) {
+        localStorage.setItem('theme', 'light_mode')
+    } else {
+        localStorage.setItem('theme', 'dark_mode')
+    }
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    let html = document.querySelector('.html')
+    if (savedTheme === 'light_mode') {
+      html.classList.add('light_mode');
+    }
+  });
